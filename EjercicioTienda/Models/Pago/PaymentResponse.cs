d44c2ee1bb8bf39b3fdcfbb095c5fd0f3c4b94d1
NB090\Leticia <letimateo@gmail.com>
2021-12-20 16:28:35 -0300

    Se agrega paymentResponse

---
+++ EjercicioTienda/Models/Pago/PaymentResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTienda.Models.Pago
+{
+    public class PaymentResponse
+    {
+        public StatusResponse status { get; set; }
+        public int requestId { get; set; }
+        public string processUrl { get; set; }
+    }
+}
---
+++ EjercicioTienda/Models/Pago/StatusResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioTienda.Models.Pago
+{
+    public class StatusResponse
+    {
+        public string status { get; set; }
+        public string reason { get; set; }
+        public string message { get; set; }
+        public DateTime date { get; set; }
+    }
+}
